09bcaac3c387e063478d6cbc71b5b0bf52ff17f1
Harman Singh <singhharman160201@gmail.com>
2021-01-16 11:56:16 +0530

    Ternary Operators Used

--- App.js
+++ App.js
@@ -1,41 +1,56 @@
-import * as React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
-import { Header } from 'react-native-elements';
-import * as Speech from 'expo-speech';
-
-
+import * as React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+} from "react-native";
+import { Header } from "react-native-elements";
+import * as Speech from "expo-speech";
 
 export default class App extends React.Component {
-
   speakWord(word) {
-    Speech.speak(word)
+    Speech.speak(word);
   }
 
   constructor() {
     super();
-    this.state = { text: '' }
+    this.state = { text: "" };
   }
   render() {
     return (
       <View>
-        <Header centerComponent={{
-          text: "Text To Speech",
-          style: { fontSize: 30 }
-        }} />
-        <TextInput style={styles.inputBox} placeholder="Type your word here" onChangeText={(text) => {
-          this.setState({ text: text })
-        }} />
-        <TouchableOpacity onPress={() => {
-          Speech.speak(this.state.text)
-          console.log(this.state.text)
-        }} style={styles.speakButton}>
-          <Text style={styles.buttonText}>
-            Speak
-          </Text>
+        <Header
+          centerComponent={{
+            text: "Text To Speech",
+            style: { fontSize: 30 },
+          }}
+        />
+        <TextInput
+          style={styles.inputBox}
+          placeholder="Type your word here"
+          onChangeText={(text) => {
+            this.setState({ text: text });
+          }}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            this.state.text === "" 
+              ? alert("No word written") 
+              : Speech.speak(this.state.text);
+            console.log(this.state.text);
+          }}
+          style={styles.speakButton}
+        >
+          <Text style={styles.buttonText}>Speak</Text>
         </TouchableOpacity>
-        <Text style={styles.text}>Well this is a simple app It says what you type in the text box and that is all !</Text>
+        <Text style={styles.text}>
+          Well this is a simple app It says what you type in the text box and
+          that is all !
+        </Text>
       </View>
-    )
+    );
   }
 }
 
@@ -50,7 +65,7 @@ const styles = StyleSheet.create({
     marginRight: 130,
     paddingRight: 20,
     paddingLeft: 20,
-    borderRadius: 60
+    borderRadius: 60,
   },
   inputBox: {
     marginTop: 50,
@@ -60,7 +75,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     borderWidth: 4,
     paddingRight: 60,
-    paddingLeft: 60
+    paddingLeft: 60,
   },
-  text: { textAlign: 'center', marginTop: 30 }
-})
+  text: { textAlign: "center", marginTop: 30 },
+});
